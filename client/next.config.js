a0d68ef113474295a742daeb01e46882ee83f9be
Sajid Shaikh <ss2727303@gmail.com>
2026-01-24 20:24:48 +0530

    Add next.config.js to fix Amplify bundling issues

---
+++ client/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // We explicitly disable standalone output for Amplify to avoid EEXIST errors
+    // Amplify handles the build artifacts itself
+    output: undefined,
+    // If you needed images from S3 later
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'coworkspace-uploads.s3.amazonaws.com',
+            },
+        ],
+    },
+};
+
+module.exports = nextConfig;
