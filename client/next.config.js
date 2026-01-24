e5b16c19d6f39395ee1337c48c5aea9e504ea9a4
Sajid Shaikh <ss2727303@gmail.com>
2026-01-24 20:50:27 +0530

    Allow Unsplash images in Next.js config

--- client/next.config.js
+++ client/next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'coworkspace-uploads.s3.amazonaws.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
         ],
     },
 };
